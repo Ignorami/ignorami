4eca7024e95f7e3ea581d0c9f98a6a3413c32d02
HAH <hhoburg@gmail.com>
2026-03-15 19:36:35 -0600

    add initial payload migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260316_013603 from './20260316_013603';
+
+export const migrations = [
+  {
+    up: migration_20260316_013603.up,
+    down: migration_20260316_013603.down,
+    name: '20260316_013603'
+  },
+];
