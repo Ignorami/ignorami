88ddd41a24676e2567cffa46af93d353a1c7059b
HAH <hhoburg@gmail.com>
2026-03-23 18:28:31 -0600

    updates third migration with do begin

--- src/migrations/20260318_221725.ts
+++ src/migrations/20260318_221725.ts
@@ -2,8 +2,18 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
+  DO $$ BEGIN
+    CREATE TYPE "public"."enum_articles_content_type" AS ENUM('article', 'short-form');
+  EXCEPTION WHEN duplicate_object THEN null;
+  END $$;
+  DO $$ BEGIN
    CREATE TYPE "public"."enum_articles_content_type" AS ENUM('article', 'short-form');
-  CREATE TYPE "public"."enum__articles_v_version_content_type" AS ENUM('article', 'short-form');
+  EXCEPTION WHEN duplicate_object THEN null;
+  END $$;
+  DO $$ BEGIN
+   CREATE TYPE "public"."enum__articles_v_version_content_type" AS ENUM('article', 'short-form');
+  EXCEPTION WHEN duplicate_object THEN null;
+  END $$;
   ALTER TABLE "articles" ADD COLUMN "content_type" "enum_articles_content_type" DEFAULT 'article';
   ALTER TABLE "_articles_v" ADD COLUMN "version_content_type" "enum__articles_v_version_content_type" DEFAULT 'article';`)
 }
