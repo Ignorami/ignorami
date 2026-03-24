d82bf13b5d40e90761fdbcae4cdac99f1f2792ef
HAH <hhoburg@gmail.com>
2026-03-23 18:31:48 -0600

    adds if not exists to fourth migration

--- src/migrations/20260320_023247.ts
+++ src/migrations/20260320_023247.ts
@@ -2,12 +2,12 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "articles" ADD COLUMN "featured" boolean DEFAULT false;
-  ALTER TABLE "_articles_v" ADD COLUMN "version_featured" boolean DEFAULT false;`)
+   ALTER TABLE "articles" ADD COLUMN IF NOT EXISTS "featured" boolean DEFAULT false;
+  ALTER TABLE "_articles_v" ADD COLUMN IF NOT EXISTS "version_featured" boolean DEFAULT false;`)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "articles" DROP COLUMN "featured";
-  ALTER TABLE "_articles_v" DROP COLUMN "version_featured";`)
+   ALTER TABLE "articles" DROP COLUMN IF EXISTS "featured";
+  ALTER TABLE "_articles_v" DROP COLUMN IF EXISTS "version_featured";`)
 }
